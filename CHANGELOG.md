88713b96735b9060256e6e07dba0d808bba82013
Narendra Bhatt <nirajbhatt1997@gmail.com>
2023-09-11 21:43:36 +0545

    fixed dropdown not following field

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,65 +1,74 @@
 # 1.0.0
 
-* Initial release.
+- Initial release.
 
 # 1.0.1
 
-* Updated screenshots.
+- Updated screenshots.
 
 # 1.0.2
 
-* Added example.
+- Added example.
 
 # 1.0.3
 
-* Added new constructor that supports fetching the data from a URL.
+- Added new constructor that supports fetching the data from a URL.
 
 # 1.0.4
 
-* Added support for dropdown field padding, border color, border width, and border radius.
-* Fixed onOptionsSelected callback not being called.
-* Added toString/JsonSerialization/Equality/Hash methods to ValueItem class.
-  
+- Added support for dropdown field padding, border color, border width, and border radius.
+- Fixed onOptionsSelected callback not being called.
+- Added toString/JsonSerialization/Equality/Hash methods to ValueItem class.
+
 # 1.0.5
 
-* Fixed onOptionsSelected callback not being called when deleting the chip item.
-* Fixed issue causing overlay exception.
-  
+- Fixed onOptionsSelected callback not being called when deleting the chip item.
+- Fixed issue causing overlay exception.
+
 # 1.0.6
 
-* Fixed selected option text color not being applied.
+- Fixed selected option text color not being applied.
 
 # 1.0.7
 
-* Added custom border radius in dropdown.
+- Added custom border radius in dropdown.
 
 # 1.0.8
 
-* Updated dropdown to detect available space and show the dropdown above or below the field.
+- Updated dropdown to detect available space and show the dropdown above or below the field.
 
 # 1.0.9
 
-* Added clear button to dropdown field.
-* Added support focus on dropdown field.
+- Added clear button to dropdown field.
+- Added support focus on dropdown field.
 
 # 2.0.0-dev.1
 
-* Added support for controller.
-* Use controller to get/set selected options, disabled options, options, and enabled options.
-* Use controller to clear selected options, show/hide dropdown.
-* Added support for focusnode, use focusnode to focus/unfocus the dropdown field programmatically.
+- Added support for controller.
+- Use controller to get/set selected options, disabled options, options, and enabled options.
+- Use controller to clear selected options, show/hide dropdown.
+- Added support for focusnode, use focusnode to focus/unfocus the dropdown field programmatically.
 
 # 2.0.0-dev.2
 
-* Fixed issue where chips were not being cleared when controller not set.
+- Fixed issue where chips were not being cleared when controller not set.
 
 # 2.0.0-dev.3
 
-* Fixed issue where selecting an option after passing initial options would not work.
+- Fixed issue where selecting an option after passing initial options would not work.
 
 # 2.0.0
-* Stable release.
+
+- Stable release.
 
 # 2.0.1
-* Added maxItems property to dropdown field. If max items is set, the dropdown field will not allow more than the specified number of items to be selected.
-* Upgraded http dependency to 1.0.0.
+
+- Added maxItems property to dropdown field. If max items is set, the dropdown field will not allow more than the specified number of items to be selected.
+- Upgraded http dependency to 1.0.0.
+
+# 2.0.2
+
+- Added support for custom suffix icon in dropdown field.
+- Added support for custom clear icon in dropdown field.
+- Fixed the issue where the dropdown would not follow the dropdown field when the dropdown field is scrolled.
+- Updated http dependency to 1.1.0.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -51,165 +52,164 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.grey.shade300,
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('WRAP', style: MyHomePage._headerStyle),
-            const SizedBox(
-              height: 4,
-            ),
-            MultiSelectDropDown(
-              showClearIcon: true,
-              controller: _controller,
-              onOptionSelected: (options) {
-                debugPrint(options.toString());
-              },
-              options: const <ValueItem>[
-                ValueItem(label: 'Option 1', value: '1'),
-                ValueItem(label: 'Option 2', value: '2'),
-                ValueItem(label: 'Option 3', value: '3'),
-                ValueItem(label: 'Option 4', value: '4'),
-                ValueItem(label: 'Option 5', value: '5'),
-                ValueItem(label: 'Option 6', value: '6'),
-              ],
-              maxItems: 4,
-              disabledOptions: const [ValueItem(label: 'Option 1', value: '1')],
-              selectionType: SelectionType.multi,
-              chipConfig: const ChipConfig(
-                  wrapType: WrapType.wrap, backgroundColor: Colors.red),
-              dropdownHeight: 300,
-              optionTextStyle: const TextStyle(fontSize: 16),
-              selectedOptionIcon: const Icon(
-                Icons.checklist,
-                color: Colors.pink,
-              ),
-              selectedOptionTextColor: Colors.blue,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Wrap(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    _controller.clearAllSelection();
-                    setState(() {
-                      _selectedOptions.clear();
-                    });
+                const Text('WRAP', style: MyHomePage._headerStyle),
+                const SizedBox(
+                  height: 4,
+                ),
+                MultiSelectDropDown(
+                  showClearIcon: true,
+                  controller: _controller,
+                  onOptionSelected: (options) {
+                    debugPrint(options.toString());
                   },
-                  child: const Text('CLEAR'),
+                  options: const <ValueItem>[
+                    ValueItem(label: 'Option 1', value: '1'),
+                    ValueItem(label: 'Option 2', value: '2'),
+                    ValueItem(label: 'Option 3', value: '3'),
+                    ValueItem(label: 'Option 4', value: '4'),
+                    ValueItem(label: 'Option 5', value: '5'),
+                    ValueItem(label: 'Option 6', value: '6'),
+                  ],
+                  maxItems: 4,
+                  selectionType: SelectionType.multi,
+                  chipConfig: const ChipConfig(
+                      wrapType: WrapType.wrap, backgroundColor: Colors.red),
+                  dropdownHeight: 300,
+                  optionTextStyle: const TextStyle(fontSize: 16),
+                  selectedOptionIcon: const Icon(
+                    Icons.check_circle,
+                    color: Colors.pink,
+                  ),
+                  suffixIcon: const Icon(Icons.dry_outlined),
+                  selectedOptionTextColor: Colors.blue,
                 ),
                 const SizedBox(
-                  width: 8,
+                  height: 50,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _selectedOptions.clear();
-                      _selectedOptions.addAll(_controller.selectedOptions);
-                    });
-                  },
-                  child: const Text('Get Selected Options'),
+                Wrap(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        _controller.clearAllSelection();
+                        setState(() {
+                          _selectedOptions.clear();
+                        });
+                      },
+                      child: const Text('CLEAR'),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _selectedOptions.clear();
+                          _selectedOptions.addAll(_controller.selectedOptions);
+                        });
+                      },
+                      child: const Text('Get Selected Options'),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_controller.isDropdownOpen) {
+                          _controller.hideDropdown();
+                        } else {
+                          _controller.showDropdown();
+                        }
+                      },
+                      child: const Text('SHOW/HIDE DROPDOWN'),
+                    ),
+                  ],
                 ),
                 const SizedBox(
-                  width: 8,
+                  height: 12,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_controller.isDropdownOpen) {
-                      _controller.hideDropdown();
-                    } else {
-                      _controller.showDropdown();
-                    }
+                Text(
+                  'Selected Options: $_selectedOptions',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text('SCROLL', style: MyHomePage._headerStyle),
+                const SizedBox(
+                  height: 4,
+                ),
+                MultiSelectDropDown(
+                  onOptionSelected: (options) {
+                    debugPrint(options.toString());
                   },
-                  child: const Text('SHOW/HIDE DROPDOWN'),
+                  options: const <ValueItem>[
+                    ValueItem(label: 'Option 1', value: '1'),
+                    ValueItem(label: 'Option 2', value: '2'),
+                    ValueItem(label: 'Option 3', value: '3'),
+                    ValueItem(label: 'Option 4', value: '4'),
+                    ValueItem(label: 'Option 5', value: '5'),
+                    ValueItem(label: 'Option 6', value: '6'),
+                  ],
+                  selectionType: SelectionType.multi,
+                  chipConfig: const ChipConfig(wrapType: WrapType.scroll),
+                  dropdownHeight: 400,
+                  optionTextStyle: const TextStyle(fontSize: 16),
+                  selectedOptionIcon: const Icon(Icons.check_circle),
                 ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text('FROM NETWORK', style: MyHomePage._headerStyle),
+                const SizedBox(
+                  height: 4,
+                ),
+                MultiSelectDropDown.network(
+                  dropdownHeight: 200,
+                  onOptionSelected: (options) {
+                    debugPrint(options.toString());
+                  },
+                  networkConfig: NetworkConfig(
+                    url: 'https://jsonplaceholder.typicode.com/users',
+                    method: RequestMethod.get,
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                  ),
+                  chipConfig: const ChipConfig(wrapType: WrapType.wrap),
+                  responseParser: (response) {
+                    debugPrint('Response: $response');
+
+                    final list = (response as List<dynamic>).map((e) {
+                      final item = e as Map<String, dynamic>;
+                      return ValueItem(
+                        label: item['name'],
+                        value: item['id'].toString(),
+                      );
+                    }).toList();
+
+                    return Future.value(list);
+                  },
+                  responseErrorBuilder: ((context, body) {
+                    return const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text('Error fetching the data'),
+                    );
+                  }),
+                ),
+                const SizedBox(height: 50)
               ],
             ),
-            const SizedBox(
-              height: 12,
-            ),
-            Text(
-              'Selected Options: $_selectedOptions',
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text('SCROLL', style: MyHomePage._headerStyle),
-            const SizedBox(
-              height: 4,
-            ),
-            MultiSelectDropDown(
-              onOptionSelected: (options) {
-                debugPrint(options.toString());
-              },
-              options: const <ValueItem>[
-                ValueItem(label: 'Option 1', value: '1'),
-                ValueItem(label: 'Option 2', value: '2'),
-                ValueItem(label: 'Option 3', value: '3'),
-                ValueItem(label: 'Option 4', value: '4'),
-                ValueItem(label: 'Option 5', value: '5'),
-                ValueItem(label: 'Option 6', value: '6'),
-              ],
-              selectionType: SelectionType.multi,
-              chipConfig: const ChipConfig(wrapType: WrapType.scroll),
-              dropdownHeight: 400,
-              optionTextStyle: const TextStyle(fontSize: 16),
-              selectedOptionIcon: const Icon(Icons.check_circle),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text('FROM NETWORK', style: MyHomePage._headerStyle),
-            const SizedBox(
-              height: 4,
-            ),
-            MultiSelectDropDown.network(
-              dropdownHeight: 200,
-              onOptionSelected: (options) {
-                debugPrint(options.toString());
-              },
-              networkConfig: NetworkConfig(
-                url: 'https://jsonplaceholder.typicode.com/users',
-                method: RequestMethod.get,
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-              ),
-              chipConfig: const ChipConfig(wrapType: WrapType.wrap),
-              responseParser: (response) {
-                debugPrint('Response: $response');
-
-                final list = (response as List<dynamic>).map((e) {
-                  final item = e as Map<String, dynamic>;
-                  return ValueItem(
-                    label: item['name'],
-                    value: item['id'].toString(),
-                  );
-                }).toList();
-
-                return Future.value(list);
-              },
-              responseErrorBuilder: ((context, body) {
-                return const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text('Error fetching the data'),
-                );
-              }),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
